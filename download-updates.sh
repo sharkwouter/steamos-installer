d5d07c1450602916190b89600e297a8eb00ad709
Wouter Wijsman <wwijsman@live.nl>
2015-01-08 15:29:02 +0100

    download-updates.sh is now functional

--- download-updates.sh
+++ download-updates.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-repo="deb http://repo.steampowered.com/steamos alchemist main"; #contrib non-free"
-#        deb http://repo.steampowered.com/steamos alchemist_beta main contrib non-free"
-architectures="amd64"
+# This script is used for downloading package updates.
+# By default, it will check the alchemist_beta repo from Valve, but it can be used for different repos.
+# Example: ./download-updates.sh deb http://repo.steampowered.com/steamos alchemist_beta main contrib non-free
+# Packages will be downloaded to the newpkgs directory.
+
+if [[ -z $@ ]]; then
+	repo="deb http://repo.steampowered.com/steamos alchemist_beta main contrib non-free"
+else
+	repo="$@"
+fi
+
+architectures="amd64 i386"
 downloaddir="newpkgs"
+
+# delete old Packages.gz file
+if [[ -f Packages.gz ]]; then
+	rm Packages.gz
+fi
 
 # do the thing for each entry in sources
 #for repo in ${sources};do
@@ -12,16 +26,27 @@ downloaddir="newpkgs"
         reponame=$(echo $repo|cut -d" " -f3)
         repoareas=$(echo $repo|cut -d" " -f4-)
         
-        # 
+        # check each part of the rpeo
         for area in ${repoareas}; do
                 for arch in ${architectures}; do
                         wget ${repourl}/dists/${reponame}/${area}/binary-${arch}/Packages.gz
-                        gunzip -c buildroot/dists/${reponame}/${area}/binary-${arch}/Packages.gz|grep Filename|cut -d" " -f2 > buildroot.txt
-                        gunzip -c Packages.gz|grep Filename|cut -d" " -f2 > repo.txt
+                        gunzip -c buildroot/dists/alchemist/${area}/binary-${arch}/Packages.gz|grep Filename|cut -d" " -f2|sort > buildroot.txt
+                        gunzip -c Packages.gz|grep Filename|cut -d" " -f2|sort > repo.txt
                         
-                        cat buildroot.txt|cut -d"_" -f1|xargs grep - repo.txt >> shared.txt 
+                        # create a list of packages which have different versions in the repo than the ones in buildroot
+                        diffpkgs=$(grep -F "`cat buildroot.txt|cut -d'_' -f1|sed 's/$/_/'`" repo.txt|grep -Fvxf "buildroot.txt")
+                        echo $diffpkgs
+                        echo "$(echo $diffpkgs|wc -w)"
                         
-                        #gunzip -c Packages.gz|grep Filename|cut -d" " -f2|grep `cat buildroot.txt|cut -d"_" -f1` > repo.txt
+                        # download all the new packages
+                        for pkg in ${diffpkgs}; do
+                        	mkdir -p ${downloaddir}
+                        	wget -nc -P ${downloaddir} ${repourl}/${pkg}
+                        done
+                        
+                        # clean up
+                        rm buildroot.txt
+                        rm repo.txt
                         rm Packages.gz
                 done
         done
